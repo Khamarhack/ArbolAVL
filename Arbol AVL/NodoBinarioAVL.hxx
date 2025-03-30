7fcb4c3f3041b7457d61c347b47091acd73141a5
Khamarhack <143459006+Khamarhack@users.noreply.github.com>
2025-03-30 11:39:11 -0500

    Update NodoBinarioAVL.hxx

--- Arbol AVL/NodoBinarioAVL.hxx
+++ Arbol AVL/NodoBinarioAVL.hxx
@@ -8,45 +8,50 @@ Estructuras de datos
 
 using namespace std;
 
+// Constructor: Inicializa los hijos como nulos
 template <class T>
-NodoBinarioAVL<T>::NodoBinarioAVL(){
-	this->hijoIzq=NULL;
-	this->hijoDer=NULL;
+NodoBinarioAVL<T>::NodoBinarioAVL() {
+    this->hijoIzq = NULL;
+    this->hijoDer = NULL;
 }
 
+// Destructor (no se requiere liberar memoria manualmente en este caso)
 template <class T>
-NodoBinarioAVL<T>::~NodoBinarioAVL(){
+NodoBinarioAVL<T>::~NodoBinarioAVL() {
 }
 
+// Devuelve el dato almacenado en el nodo
 template <class T>
-T& NodoBinarioAVL<T>::getDato(){
-	return this->dato;
+T& NodoBinarioAVL<T>::getDato() {
+    return this->dato;
 }
 
+// Asigna un valor al nodo
 template <class T>
-void NodoBinarioAVL<T>::setDato(T& val){
-	this->dato=val;
-	return;
+void NodoBinarioAVL<T>::setDato(T& val) {
+    this->dato = val;
 }
 
+// Devuelve el puntero al hijo izquierdo
 template <class T>
-NodoBinarioAVL<T>* NodoBinarioAVL<T>::getHijoIzq(){
-	return this->hijoIzq;
+NodoBinarioAVL<T>* NodoBinarioAVL<T>::getHijoIzq() {
+    return this->hijoIzq;
 }
 
+// Devuelve el puntero al hijo derecho
 template <class T>
-NodoBinarioAVL<T>* NodoBinarioAVL<T>::getHijoDer(){
-	return this->hijoDer;
+NodoBinarioAVL<T>* NodoBinarioAVL<T>::getHijoDer() {
+    return this->hijoDer;
 }
 
+// Asigna un nodo como hijo izquierdo
 template <class T>
-void NodoBinarioAVL<T>::setHijoIzq(NodoBinarioAVL<T>* izq){
-	this->hijoIzq=izq;
-	return;
+void NodoBinarioAVL<T>::setHijoIzq(NodoBinarioAVL<T>* izq) {
+    this->hijoIzq = izq;
 }
 
+// Asigna un nodo como hijo derecho
 template <class T>
-void NodoBinarioAVL<T>::setHijoDer(NodoBinarioAVL<T>* der){
-	this->hijoDer=der;
-	return;
+void NodoBinarioAVL<T>::setHijoDer(NodoBinarioAVL<T>* der) {
+    this->hijoDer = der;
 }
