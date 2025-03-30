29ed4d2efb12c7f468dd93d4174cf0817d0be05c
Khamarhack <143459006+Khamarhack@users.noreply.github.com>
2025-03-30 11:38:16 -0500

    Update NodoBinarioAVL.h

--- Arbol AVL/NodoBinarioAVL.h
+++ Arbol AVL/NodoBinarioAVL.h
@@ -7,25 +7,29 @@ Estructuras de datos
 #ifndef NODOBINARIOAVL_H
 #define NODOBINARIOAVL_H
 
+// Clase genérica para representar un nodo en un árbol AVL
 template <class T>
+class NodoBinarioAVL {
+    public:
+        T dato;  // Valor almacenado en el nodo
+        NodoBinarioAVL<T> *hijoIzq;  // Puntero al hijo izquierdo
+        NodoBinarioAVL<T> *hijoDer;  // Puntero al hijo derecho
 
-class NodoBinarioAVL{
-	public:
-		T dato;
-		NodoBinarioAVL<T> *hijoIzq;
-		NodoBinarioAVL<T> *hijoDer;
+    public:
+        NodoBinarioAVL();  // Constructor
+        ~NodoBinarioAVL();  // Destructor
 
-	public:
-		NodoBinarioAVL();
-		~NodoBinarioAVL();
-		T& getDato();
-		void setDato(T& val);
-		NodoBinarioAVL<T>* getHijoIzq();
-		NodoBinarioAVL<T>* getHijoDer();
-		void setHijoIzq(NodoBinarioAVL<T>* izq);
-		void setHijoDer(NodoBinarioAVL<T>* der);
+        T& getDato();  // Obtener el dato del nodo
+        void setDato(T& val);  // Asignar un valor al nodo
+
+        NodoBinarioAVL<T>* getHijoIzq();  // Obtener el hijo izquierdo
+        NodoBinarioAVL<T>* getHijoDer();  // Obtener el hijo derecho
+
+        void setHijoIzq(NodoBinarioAVL<T>* izq);  // Asignar hijo izquierdo
+        void setHijoDer(NodoBinarioAVL<T>* der);  // Asignar hijo derecho
 };
 
+// Se incluye la implementación de los métodos en el archivo .hxx
 #include "NodoBinarioAVL.hxx"
 
-#endif //NODOBINARIOAVL_H
+#endif // NODOBINARIOAVL_H
