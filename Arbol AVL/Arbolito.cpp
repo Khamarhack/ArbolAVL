d95029925930ade3eb8ca2c61fb50104bd72f3c1
Khamarhack <143459006+Khamarhack@users.noreply.github.com>
2025-03-30 11:37:19 -0500

    Update Arbolito.cpp

--- Arbol AVL/Arbolito.cpp
+++ Arbol AVL/Arbolito.cpp
@@ -4,33 +4,38 @@ Autor: Santiago Camargo
 Tema: Taller Arboles
 Estructuras de datos
 ******************************************/
-#include<iostream>
+#include <iostream>
 #include "ArbolAVL.h"
 #include "NodoAVL.h"
 
 using namespace std;
 
-main(int argc, char* argv[])
-{
-    ArbolAVL<int> arbolito;
-	while(true)
-	{
-		int dato;
-		char op;
-		cin>>op>>dato;
-		
-		if(op == 'A')
-			arbolito.insertar(dato);
-		else if(op == 'E')
-			arbolito.eliminar(dato);
-		else
-			break;
-	}
-	
-	cout << endl << "Inorden: " << endl;
-	arbolito.inOrden(arbolito.getRaiz());
-	cout << endl << "Preorden: " << endl;
-	arbolito.preOrden(arbolito.getRaiz());
-	cout << endl <<"Posorden: " << endl;
-	arbolito.posOrden(arbolito.getRaiz());
+int main(int argc, char* argv[]) {
+    ArbolAVL<int> arbolito;  // Se crea un árbol AVL de enteros
+
+    while (true) {
+        int dato;
+        char op;
+        cin >> op >> dato;  // Se lee la operación y el dato
+
+        // Se realizan operaciones en el árbol según la entrada del usuario
+        if (op == 'A')  
+            arbolito.insertar(dato);  // Agregar un elemento
+        else if (op == 'E')  
+            arbolito.eliminar(dato);  // Eliminar un elemento
+        else  
+            break;  // Finalizar el ciclo si la entrada no es válida
+    }
+
+    // Imprimir los recorridos del árbol
+    cout << endl << "Inorden: " << endl;
+    arbolito.inOrden(arbolito.getRaiz());
+
+    cout << endl << "Preorden: " << endl;
+    arbolito.preOrden(arbolito.getRaiz());
+
+    cout << endl << "Posorden: " << endl;
+    arbolito.posOrden(arbolito.getRaiz());
+
+    return 0;  // Fin del programa
 }
